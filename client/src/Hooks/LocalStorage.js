04db4bd825f11aec28da0cbbbeabb97129523ba2
Oluwa-femi <oluwatolablessing@gmail.com>
2019-10-04 11:40:58 +0100

    Got my styling and hooks working

--- client/src/Hooks/LocalStorage.js
+++ client/src/Hooks/LocalStorage.js
@@ -1,16 +1,16 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// export const useLocalStorage = (key, initialValue) => {
+export const useLocalStorage = (key, initialValue) => {
 
-//     const [storedValue, setStoredValue] = useState(() => {
+    const [storedValue, setStoredValue] = useState(() => {
 
-//         const item = window.localStorage.getItem(key);
-//         return item ? JSON.parse(item) : initialValue;
-//     });
-//     const setValue = value => {
-//         setStoredValue(value);
-//         window.localStorage.setItem(key, JSON.stringify(value));
-//     }
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    });
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
 
-//     return [storedValue, setValue];
-// }
+    return [storedValue, setValue];
+} 
